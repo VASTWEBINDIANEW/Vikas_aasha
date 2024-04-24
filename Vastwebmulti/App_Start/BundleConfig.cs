914eedf08d88f34d6dc855764421e07d9cf2cdfe
ajayhit <ajayhit@hotmail.com>
2024-04-24 10:36:01 +0530

    firest

--- Vastwebmulti/App_Start/BundleConfig.cs
+++ Vastwebmulti/App_Start/BundleConfig.cs
@@ -4,6 +4,7 @@ namespace Vastwebmulti
 {
     public class BundleConfig
     {
+
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
